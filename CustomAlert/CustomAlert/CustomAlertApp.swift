cdacf7ca98deb1888bce61204608daaefb2363e4
Jesús Antonio Gil <jesus.antoniogil@telefonica.com>
2025-02-14 12:54:27 +0100

    Create project

---
+++ CustomAlert/CustomAlert/CustomAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomAlertApp.swift
+//  CustomAlert
+//
+//  Created by Jesus Antonio Gil on 14/2/25.
+//
+
+import SwiftUI
+
+@main
+struct CustomAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
