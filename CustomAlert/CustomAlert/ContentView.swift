0cf9209bab4c5a703570bcbf34cba59d4d8eff23
Jesús Antonio Gil <jesus.antoniogil@telefonica.com>
2025-02-14 21:32:23 +0100

    Added Custom Alert Code

--- CustomAlert/CustomAlert/ContentView.swift
+++ CustomAlert/CustomAlert/ContentView.swift
@@ -7,18 +7,71 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State private var showAlert = false
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                Button("Show Alert") {
+                    showAlert.toggle()
+                }
+                .alert(isPresented: $showAlert) {
+                    VStack(spacing: 15) {
+                        Text("Hello I`m Alert!")
+                            .fontWeight(.semibold)
+                            .underline()
+                        
+                        Button("Dismiss") {
+                            showAlert.toggle()
+                        }
+                        .buttonStyle(.borderedProminent)
+                        .tint(.blue)
+                        .buttonBorderShape(.roundedRectangle(radius: 10))
+                    }
+                    .padding(15)
+                    .background(.background, in: .rect(cornerRadius: 15))
+                    .transition(.blurReplace.combined(with: .push(from: .bottom)))
+                } background: {
+                    Rectangle()
+                        .fill(.primary.opacity(0.35))
+                }
+
+            }
+            .navigationTitle("Custom Alert")
         }
-        .padding()
     }
 }
 
+
 #Preview {
     ContentView()
+}
+
+
+struct CustomDialog: View {
+    var title: String
+    var content: String?
+    var image: Config
+    var button1: Config
+    var button2: Config?
+    var addTextField: Bool = false
+    var textFieldHint: String = ""
+    
+    @State private var text: String
+    
+    
+    var body: some View {
+        
+    }
+    
+    
+    struct Config {
+        var content: String
+        var tint: Color
+        var foreground: Color
+        var action: (String) -> () = { _ in }
+    }
 }
